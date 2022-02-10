30800a669a87268ea63140d11d1736b6b60618ad
omshree vasant tidke <86835787+omshreetidke@users.noreply.github.com>
2022-02-10 15:59:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+
+var trex ,trex_running;
+function preload(){
+  
+trex_running=loadAnimation("trex1.png","trex3.png","trex4.png");
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+  trex=createSprite(50,150,20,50);
+  ground=createSprite(200,180,400,20)
+ trex.addAnimation("running",trex_running);
+ trex.scale=0.5
+}
+
+function draw(){
+  background("white")
+if(keyDown("space")){
+trex.velocityY=-10
+}
+trex.velocityY=trex.velocityY+0.5
+trex.collide(ground)
+  drawSprites()
+
+
+}
